6b4f5fac5d52162293bc3c9af76659fe54c22009
RollW <73376341+Roll-W@users.noreply.github.com>
2024-01-27 21:13:32 +0800

    feat: add database.

---
+++ app/src/main/java/tech/rollw/player/data/database/PlayerDatabase.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2024 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.rollw.player.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+import kotlin.concurrent.Volatile
+
+/**
+ * @author RollW
+ */
+@Database(
+    version = 1
+)
+abstract class PlayerDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PlayerDatabase? = null
+
+        const val DATABASE_NAME = "player_database"
+
+        fun getDatabase(context: Context): PlayerDatabase {
+            if (INSTANCE == null) {
+                synchronized(PlayerDatabase::class) {
+                    if (INSTANCE == null) {
+                        INSTANCE = databaseBuilder(
+                            context.applicationContext, PlayerDatabase::class.java,
+                            DATABASE_NAME
+                        ).allowMainThreadQueries()
+                            .enableMultiInstanceInvalidation()
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    }
+                }
+            }
+            return INSTANCE!!
+        }
+
+        fun deleteDatabase(context: Context) {
+            INSTANCE = null
+            context.deleteDatabase(DATABASE_NAME)
+        }
+    }
+}
