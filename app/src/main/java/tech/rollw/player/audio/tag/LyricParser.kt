2a7ab2d2b6d3ba41e95a800c9612362307a56807
RollW <73376341+Roll-W@users.noreply.github.com>
2024-03-01 18:03:26 +0800

    feat: add lyric parser.

---
+++ app/src/main/java/tech/rollw/player/audio/tag/LyricParser.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2024 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.rollw.player.audio.tag
+
+import java.io.File
+import java.util.regex.Pattern
+
+/**
+ * @author RollW
+ */
+class LyricParser(
+    lyric: String
+) {
+    private val lines = lyric.lines()
+
+    private val rows = mutableListOf<LyricRow>()
+
+    fun parse(): List<LyricRow> {
+        if (rows.isNotEmpty()) {
+            return rows
+        }
+        val parsed = mutableListOf<LyricRow>()
+        lines.forEach {
+            val row = parseLine(it)
+            if (row != null) {
+                parsed.add(row)
+            }
+        }
+        rows.addAll(collapseRows(parsed))
+        return rows
+    }
+
+    private fun collapseRows(rows: MutableList<LyricRow>): List<LyricRow> {
+        rows.sort()
+        val collapsed = mutableListOf<LyricRow>()
+        val timeLyricMap = hashMapOf<Long, LyricRow>()
+        rows.forEach {
+            val row = timeLyricMap[it.timestamp]
+            if (row == null) {
+                timeLyricMap[it.timestamp] = it
+            } else {
+                row += it
+            }
+        }
+
+        timeLyricMap.forEach { (_, u) ->
+            collapsed.add(u)
+        }
+        return collapsed.sorted()
+    }
+
+    private fun parseLine(line: String): LyricRow? {
+        if (line.isEmpty()) {
+            return null
+        }
+        val timestampMatcher = TIMESTAMP_PATTEN.matcher(line)
+        if (!timestampMatcher.find()) {
+            return LyricRow(line, LyricRow.INVALID_TIMESTAMP, line)
+        }
+        val timestamp = parseTimestamp(timestampMatcher.group())
+        if (timestamp < 0) {
+            return LyricRow(line, LyricRow.INVALID_TIMESTAMP, line)
+        }
+        val lyric = line.substring(timestampMatcher.end())
+        return LyricRow(line, timestamp, lyric)
+    }
+
+    private fun parseTimestamp(timestamp: String): Long {
+        val times = timestamp
+            .removePrefix("[")
+            .removeSuffix("]")
+            .replace('.', ':')
+            .split(":")
+            .toTypedArray()
+        if (times.size < 3) {
+            return LyricRow.INVALID_TIMESTAMP
+        }
+        // mm:ss:SS
+        return try {
+            times[0].toInt() * 60 * 1000L +
+                    times[1].toInt() * 1000 +
+                    times[2].toInt()
+        } catch (e: NumberFormatException) {
+            LyricRow.INVALID_TIMESTAMP
+        }
+    }
+
+    companion object {
+        private const val TAG = "LyricParser"
+
+        private const val TIMESTAMP_REGEX = "\\[\\d*?:\\d*\\.\\d*]"
+        private val TIMESTAMP_PATTEN: Pattern = Pattern.compile(TIMESTAMP_REGEX)
+
+
+        fun createFrom(audioTag: AudioTag): LyricParser? {
+            val lyric = audioTag.getTagField(AudioTagField.LYRICS) ?: return null
+            return LyricParser(lyric)
+        }
+
+        fun createFrom(file: File): LyricParser {
+            return LyricParser(file.readText())
+        }
+
+        fun createFrom(lyric: String): LyricParser {
+            return LyricParser(lyric)
+        }
+    }
+
+}
---
+++ app/src/main/java/tech/rollw/player/audio/tag/LyricRow.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2024 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.rollw.player.audio.tag
+
+/**
+ * @author RollW
+ */
+class LyricRow(
+    private val raw: String,
+    val timestamp: Long = INVALID_TIMESTAMP,
+    contents: List<String>
+) : Comparable<LyricRow> {
+    constructor(raw: String, timestamp: Long, content: String) :
+            this(raw, timestamp, listOf(content))
+
+    private val _contents = mutableListOf<String>()
+
+    val contents: List<String>
+        get() = _contents
+
+    init {
+        _contents.addAll(contents)
+    }
+
+    override fun compareTo(other: LyricRow) =
+        timestamp.compareTo(other.timestamp)
+
+    operator fun get(index: Int) = contents[index]
+
+    operator fun plusAssign(lrc: LyricRow) {
+        if (lrc.timestamp != timestamp) {
+            throw IllegalArgumentException("Timestamp not equal.")
+        }
+
+        _contents.addAll(lrc.contents)
+    }
+
+    operator fun plus(content: String) =
+        LyricRow(raw, timestamp, _contents + content)
+
+    operator fun plusAssign(content: String) {
+        _contents.add(content)
+    }
+
+    operator fun plusAssign(contents: List<String>) {
+        _contents.addAll(contents)
+    }
+
+    operator fun minusAssign(contents: List<String>) {
+        _contents.removeAll(contents)
+    }
+
+    operator fun minusAssign(content: String) {
+        _contents.remove(content)
+    }
+
+    operator fun contains(content: String) = content in _contents
+
+    override fun toString(): String {
+        return "LyricRow(raw='$raw', time=$timestamp, contents=$_contents)"
+    }
+
+    companion object {
+        const val INVALID_TIMESTAMP = -1L
+    }
+
+}
