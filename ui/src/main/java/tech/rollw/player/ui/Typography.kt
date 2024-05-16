42940208dcf6fde35f6f209a4378879bf64a0169
RollW <73376341+Roll-W@users.noreply.github.com>
2024-05-16 21:49:44 +0800

    feat(ui): add FontUnits field to ContentTypography.

--- ui/src/main/java/tech/rollw/player/ui/Typography.kt
+++ ui/src/main/java/tech/rollw/player/ui/Typography.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.unit.sp
 import tech.rollw.compose.ui.text.FontUnit
 import tech.rollw.compose.ui.text.FontUnit.Companion.lineHeight
 import tech.rollw.compose.ui.text.TextStyle
+import tech.rollw.compose.ui.text.copy
 
 /**
  * @author RollW
@@ -34,9 +35,22 @@ data class Typography(
     val contentLarge: ContentTypography = TypographyDefaults.ContentDefault,
     val contentNormal: ContentTypography = TypographyDefaults.ContentDefault,
     val contentSmall: ContentTypography = TypographyDefaults.ContentDefault,
+    /**
+     * The default font units for the typography.
+     */
     val fontUnits: FontUnits = TypographyDefaults.FontUnits
 )
 
+enum class TypographyTokens {
+    Header,
+    Title,
+    Subtitle,
+    Body,
+    Info,
+    Tag,
+    Tip,
+    Code
+}
 
 /**
  * Represents the typography for the content in the app.
@@ -68,13 +82,90 @@ data class ContentTypography(
     val info: TextStyle = TypographyDefaults.Info,
     val tag: TextStyle = TypographyDefaults.Tag,
     val tip: TextStyle = TypographyDefaults.Tip,
-    val code: TextStyle = TypographyDefaults.Code
-)
+    val code: TextStyle = TypographyDefaults.Code,
+    val fontUnits: FontUnits = TypographyDefaults.FontUnits
+) {
+    /**
+     * Apply the given [fontUnit] to all text styles.
+     *
+     * Default values are:
+     * - [header] = [FontUnitTokens.Huge]
+     * - [title] = [FontUnitTokens.ExtraLarge]
+     * - [subtitle] = [FontUnitTokens.Large]
+     * - [body] = [FontUnitTokens.Normal]
+     * - [info] = [FontUnitTokens.Small]
+     * - [tag] = [FontUnitTokens.Normal]
+     * - [tip] = [FontUnitTokens.Normal]
+     * - [code] = [FontUnitTokens.Normal]
+     */
+    fun applyFontUnit(fontUnit: FontUnits): ContentTypography = copy(
+        header = header.copy(fontUnit = fontUnit[FontUnitTokens.Huge]),
+        title = title.copy(fontUnit = fontUnit[FontUnitTokens.ExtraLarge]),
+        subtitle = subtitle.copy(fontUnit = fontUnit[FontUnitTokens.Large]),
+        body = body.copy(fontUnit = fontUnit[FontUnitTokens.Normal]),
+        info = info.copy(fontUnit = fontUnit[FontUnitTokens.Small]),
+        tag = tag.copy(fontUnit = fontUnit[FontUnitTokens.Normal]),
+        tip = tip.copy(fontUnit = fontUnit[FontUnitTokens.Normal]),
+        code = code.copy(fontUnit = fontUnit[FontUnitTokens.Normal]),
+        fontUnits = fontUnit
+    )
+
+    operator fun get(token: TypographyTokens): TextStyle = when (token) {
+        TypographyTokens.Header -> header
+        TypographyTokens.Title -> title
+        TypographyTokens.Subtitle -> subtitle
+        TypographyTokens.Body -> body
+        TypographyTokens.Info -> info
+        TypographyTokens.Tag -> tag
+        TypographyTokens.Tip -> tip
+        TypographyTokens.Code -> code
+    }
+
+    companion object {
+        fun createFromBaselineStyle(
+            textStyle: TextStyle, fontUnits: FontUnits,
+            mapping: Map<TypographyTokens, FontUnitTokens> = Mapping
+        ): ContentTypography {
+
+            fun TextStyle.copy(fontUnit: FontUnitTokens): TextStyle = copy(
+                fontUnit = fontUnits[fontUnit]
+            )
+
+            fun getOrDefault(
+                mapping: Map<TypographyTokens, FontUnitTokens>,
+                token: TypographyTokens
+            ): FontUnitTokens = mapping[token] ?: Mapping.getValue(token)
+
+            return ContentTypography(
+                header = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Header)),
+                title = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Title)),
+                subtitle = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Subtitle)),
+                body = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Body)),
+                info = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Info)),
+                tag = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Tag)),
+                tip = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Tip)),
+                code = textStyle.copy(fontUnit = getOrDefault(mapping, TypographyTokens.Code)),
+                fontUnits = fontUnits
+            )
+        }
+
+        val Mapping = mapOf(
+            TypographyTokens.Header to FontUnitTokens.Huge,
+            TypographyTokens.Title to FontUnitTokens.ExtraLarge,
+            TypographyTokens.Subtitle to FontUnitTokens.Large,
+            TypographyTokens.Body to FontUnitTokens.Normal,
+            TypographyTokens.Info to FontUnitTokens.Small,
+            TypographyTokens.Tag to FontUnitTokens.Normal,
+            TypographyTokens.Tip to FontUnitTokens.Normal,
+            TypographyTokens.Code to FontUnitTokens.Normal
+        )
+    }
+}
 
 data class FontUnits(
-    val extraSmall: FontUnit = 10.sp lineHeight 14.sp,
-    val small: FontUnit = 12.sp lineHeight 16.sp,
-    val normal: FontUnit = 14.sp lineHeight 20.sp,
+    val tiny: FontUnit = 10.sp lineHeight 16.sp,
+    val small: FontUnit = 12.sp lineHeight 18.sp,
+    val normal: FontUnit = 14.sp lineHeight 22.sp,
     val large: FontUnit = 16.sp lineHeight 24.sp,
     val extraLarge: FontUnit = 18.sp lineHeight 28.sp,
     val extremeLarge: FontUnit = 20.sp lineHeight 32.sp,
@@ -82,7 +173,7 @@ data class FontUnits(
     val extraHuge: FontUnit = 28.sp lineHeight 40.sp
 ) {
     operator fun get(unit: FontUnitTokens): FontUnit = when (unit) {
-        FontUnitTokens.ExtraSmall -> extraSmall
+        FontUnitTokens.Tiny -> tiny
         FontUnitTokens.Small -> small
         FontUnitTokens.Normal -> normal
         FontUnitTokens.Large -> large
@@ -93,8 +184,9 @@ data class FontUnits(
     }
 }
 
+
 enum class FontUnitTokens {
-    ExtraSmall,
+    Tiny,
     Small,
     Normal,
     Large,
@@ -110,7 +202,7 @@ internal object TypographyDefaults {
     val Header = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
-        fontUnit = FontUnits.extremeLarge,
+        fontUnit = FontUnits.huge,
         letterSpacing = 0.5.sp
     )
     val Title = TextStyle(
@@ -140,13 +232,13 @@ internal object TypographyDefaults {
     val Tip = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontUnit = FontUnits.small,
+        fontUnit = FontUnits.normal,
         letterSpacing = 0.5.sp
     )
     val Tag = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontUnit = FontUnits.small,
+        fontUnit = FontUnits.normal,
         letterSpacing = 0.5.sp
     )
     val Code = TextStyle(
