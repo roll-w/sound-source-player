d23b48d74f16287b0e8bc5f8dbbb353fb2e6791f
RollW <73376341+Roll-W@users.noreply.github.com>
2024-05-20 21:23:00 +0800

    build: add opt-in args in kotlinOptions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,6 +143,13 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.foundation.layout.ExperimentalLayoutApi",
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+            "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
+            "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+            "-opt-in=androidx.compose.animation.ExperimentalAnimationApi",
+        )
     }
     buildFeatures {
         buildConfig = true
