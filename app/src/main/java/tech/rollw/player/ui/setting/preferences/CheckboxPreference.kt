b6c313df5709a17a77e4ef7108c9be66a5743417
RollW <73376341+Roll-W@users.noreply.github.com>
2024-06-04 15:57:19 +0800

    feat: add composable component CheckboxPreference.

---
+++ app/src/main/java/tech/rollw/player/ui/setting/preferences/CheckboxPreference.kt
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2024 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.rollw.player.ui.setting.preferences
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.Role
+import tech.rollw.compose.foundation.layout.copy
+import tech.rollw.player.ui.ContentTypography
+import tech.rollw.player.ui.LocalContentTypography
+import tech.rollw.player.ui.PlayerTheme
+
+fun PreferenceScreenScope.checkboxPreference(
+    state: MutableState<Boolean>,
+    title: @Composable (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    key: String? = null,
+    enabled: Boolean = true,
+    padding: PaddingValues = PreferenceDefaults.PaddingValues,
+    shape: Shape = RectangleShape,
+    icon: @Composable ((Boolean) -> Unit)? = null,
+    summary: @Composable ((Boolean) -> Unit)? = null
+) {
+    item(key = key, contentType = "CheckboxPreference") {
+        val contentTypography = LocalContentTypography.current
+        val value by state
+        CheckboxPreference(
+            state = state,
+            title = { title(value) },
+            modifier = modifier,
+            enabled = enabled,
+            padding = padding,
+            contentTypography = contentTypography,
+            shape = shape,
+            icon = icon?.let { { icon(value) } },
+            summary = summary?.let { { summary(value) } }
+        )
+    }
+}
+
+@Composable
+fun CheckboxPreference(
+    state: MutableState<Boolean>,
+    title: @Composable (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    padding: PaddingValues = PreferenceDefaults.PaddingValues,
+    contentTypography: ContentTypography = PlayerTheme.typography.contentMedium,
+    shape: Shape = RectangleShape,
+    icon: @Composable ((Boolean) -> Unit)? = null,
+    summary: @Composable ((Boolean) -> Unit)? = null,
+    bottomWidget: @Composable ((Boolean) -> Unit)? = null
+) {
+    var value by state
+    CheckboxPreference(
+        value = value,
+        onValueChange = { value = it },
+        title = { title(value) },
+        modifier = modifier,
+        enabled = enabled,
+        padding = padding,
+        contentTypography = contentTypography,
+        shape = shape,
+        icon = icon?.let { { icon(value) } },
+        summary = summary?.let { { summary(value) } },
+        bottomWidget = bottomWidget?.let { { bottomWidget(value) } }
+    )
+}
+
+@Composable
+fun CheckboxPreference(
+    value: Boolean,
+    onValueChange: (Boolean) -> Unit,
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    padding: PaddingValues = PreferenceDefaults.PaddingValues,
+    contentTypography: ContentTypography = PlayerTheme.typography.contentMedium,
+    shape: Shape = RectangleShape,
+    icon: @Composable (() -> Unit)? = null,
+    summary: @Composable (() -> Unit)? = null,
+    bottomWidget: @Composable (() -> Unit)? = null
+) {
+    Preference(
+        title = title,
+        modifier = modifier
+            .clip(shape)
+            .toggleable(
+                value = value,
+                enabled = enabled,
+                role = Role.Checkbox,
+                onValueChange = onValueChange
+            ),
+        enabled = enabled,
+        padding = padding,
+        contentTypography = contentTypography,
+        icon = icon,
+        summary = summary,
+        endWidget = {
+            Checkbox(
+                checked = value,
+                onCheckedChange = null,
+                modifier = Modifier.padding(
+                    padding.copy(start = PreferenceDefaults.Padding)
+                ),
+                enabled = enabled
+            )
+        },
+        bottomWidget = bottomWidget
+    )
+}
